76eceec2e76b42121d1b35c318bd0165030f3ce8
Lucas Andrade <lucasluis.prog@gmail.com>
2024-04-04 17:44:04 -0300

    Registrando model na area administrativa do django e customizando opções admin.ModelAdmin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from contact.models import Contact
 
-# Register your models here.
+
+# é necessario registrar o model
+# configuração do model na admin do django
+# uma das formas é com o decorator @admin do django
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = '-id',  # id decrescente
+    # list_filter = 'created_data', 
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'phone',
+    
